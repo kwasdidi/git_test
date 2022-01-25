5c07246a632061200e48e0d62632d3338111486b
kwas <2971826660@qq.com>
2022-01-25 18:12:29 +0800

    third commit by lzx

--- src/main/java/com/kwas/GitTest.java
+++ src/main/java/com/kwas/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println(1111);
         System.out.println("hello git!");
+        System.out.println("dev hello");
     }
 }
