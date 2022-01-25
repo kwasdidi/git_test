e4d7a05a532b4674de37cc7bde23ba3a966219f5
kwas <2971826660@qq.com>
2022-01-25 15:59:40 +0800

    first commit by lzx

---
+++ src/main/java/com/kwas/GitTest.java
@@ -0,0 +1,12 @@
+package com.kwas;
+
+/**
+ * @Author LZX
+ * @Date 25/1/2022 下午 3:57
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
